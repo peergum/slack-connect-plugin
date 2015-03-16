jenkinsci/slack-plugin

Plugin rename to slack-connect updated README, created wiki

src/main/java/jenkins/plugins/slack_connect/DisabledNotifier.java
@@ -1,4 +1,4 @@
-package jenkins.plugins.slack;
+package jenkins.plugins.slack_connect;
 
 import hudson.model.AbstractBuild;
 

src/main/java/jenkins/plugins/slack_connect/SlackListener.java
@@ -1,4 +1,4 @@
-package jenkins.plugins.slack;
+package jenkins.plugins.slack_connect;
 
 import hudson.Extension;
 import hudson.model.AbstractBuild;

src/main/java/jenkins/plugins/slack_connect/SlackService.java
@@ -1,4 +1,4 @@
-package jenkins.plugins.slack;
+package jenkins.plugins.slack_connect;
 
 public interface SlackService {
     void publish(String message);

src/main/java/jenkins/plugins/slack_connect/StandardSlackService.java
@@ -1,4 +1,4 @@
-package jenkins.plugins.slack;
+package jenkins.plugins.slack_connect;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus;

src/test/java/jenkins/plugins/slack_connect/StandardSlackServiceTest.java
@@ -1,13 +1,13 @@
-package jenkins.plugins.slack;
+package jenkins.plugins.slack_connect;
 
-import jenkins.plugins.slack.StandardSlackService;
+import org.jvnet.hudson.test.JenkinsRule;
 import org.junit.Before;
 import org.junit.Test;
 
 public class StandardSlackServiceTest {
 
     /**
-     * Publish should generally not rethrow exceptions, or it will cause a build job to fail at end.
+     * Publish should generally not re-throw exceptions, or it will cause a build job to fail at end.
      */
     @Test
     public void publishWithBadHostShouldNotRethrowExceptions() {

peergum/slack-connect-plugin

src/main/java/jenkins/plugins/slackConnect/DisabledNotifier.java
@@ -1,4 +1,4 @@
-package jenkins.plugins.slack;
+package jenkins.plugins.slackConnect;
 
 import hudson.model.AbstractBuild;
 

src/main/java/jenkins/plugins/slackConnect/SlackListener.java
@@ -1,4 +1,4 @@
-package jenkins.plugins.slack;
+package jenkins.plugins.slackConnect;
 
 import hudson.Extension;
 import hudson.model.AbstractBuild;

src/main/java/jenkins/plugins/slackConnect/SlackService.java
@@ -1,4 +1,4 @@
-package jenkins.plugins.slack;
+package jenkins.plugins.slackConnect;
 
 public interface SlackService {
     void publish(String message);

src/main/java/jenkins/plugins/slackConnect/StandardSlackService.java
@@ -1,4 +1,4 @@
-package jenkins.plugins.slack;
+package jenkins.plugins.slackConnect;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus;

src/test/java/jenkins/plugins/slackConnect/StandardSlackServiceTest.java
@@ -1,13 +1,13 @@
-package jenkins.plugins.slack;
+package jenkins.plugins.slackConnect;
 
-import jenkins.plugins.slack.StandardSlackService;
+import org.jvnet.hudson.test.JenkinsRule;
 import org.junit.Before;
 import org.junit.Test;
 
 public class StandardSlackServiceTest {
 
     /**
-     * Publish should generally not rethrow exceptions, or it will cause a build job to fail at end.
+     * Publish should generally not re-throw exceptions, or it will cause a build job to fail at end.
      */
     @Test
     public void publishWithBadHostShouldNotRethrowExceptions() {